50c990d6d92c6d70291316a37f7c64806ca7b178
Zaw Than <zawlurnthan@gmail.com>
2022-05-24 22:01:00 -0700

    Initial Application Setup

---
+++ src/main/java/Collection/App.java
@@ -0,0 +1,42 @@
+package Collection;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Zaw L Than
+ */
+public class App {
+    public static void main(String[] args) {
+        
+    }
+}
+
+// Step 1
+// List Requirements
+ 
+// Step 2
+// Design (Identify the required Classes)
+//  DATA
+//      1. DTO
+//      2. DAO
+//  View
+//      3. UserIO
+//      4. View
+//  Controller
+//      5. controller
+//      7. AppMain
+
+
+// Step 3
+// Define properties and methods for each class.
+
+// Step 4
+// Formatting database or files
+
+// Step 5
+// Construction
